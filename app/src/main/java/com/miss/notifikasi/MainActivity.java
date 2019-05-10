02aa33ae84e704599beea5ed15b71a0342720fc8
MISBAH <misbah@MacBook-Pro-1.local>
2019-05-10 08:40:58 +0700

    Initial commit

---
+++ app/src/main/java/com/miss/notifikasi/MainActivity.java
@@ -0,0 +1,54 @@
+package com.miss.notifikasi;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    // identititas id unik untuk notification
+    public static final int NOTIFICATION_ID = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getSupportActionBar().setSubtitle("BY: MISBAH BAGASKARA PURWANTO");
+
+    }
+
+    public void tampilNotification(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://misbahbagaskara.blogspot.com/"));
+        //menginisialiasasi intent
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        //untuk memanggil activity di Notification
+        /*
+Menmbangun atau mensetup Notification dengan NotificationCompat.Builder
+ */
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher) //ikon notification
+                .setContentTitle("1 Postingan Baru")//judul konten
+                .setContentIntent(pendingIntent)//memanggil object pending intent
+                .setAutoCancel(true)//untuk menswipe atau menghapus notification
+                .setContentText("Kunjungi blog misbahbagaskara.blogspot.com"); //isi text
+
+/*
+Kemudian kita harus menambahkan Notification dengan menggunakan NotificationManager
+ */
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build()
+        );
+    }
+
+}
